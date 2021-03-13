7ee551403980c4399f1d53b82c1a532f17c04fb5
Gaurav Burhade <48740774+gburhade@users.noreply.github.com>
2021-03-13 16:45:09 +0530

    Create NewMerge.java

---
+++ src/main/java/SeleniumSessions/NewMerge.java
@@ -0,0 +1,16 @@
+package SeleniumSessions;
+
+public class NewMerge {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Remote  -  Merge");
+
+	}
+	
+	public void Login()
+	{
+		System.out.println("Remote   -   Login");
+	}
+
+}
