7f3c2aa3b37d76d9e424dace8ef663dab3f62ab5
Gaurav Burhade <48740774+gburhade@users.noreply.github.com>
2021-03-13 16:55:18 +0530

    Create LaunchAppTest.java

---
+++ src/main/java/SeleniumSessions/LaunchAppTest.java
@@ -0,0 +1,18 @@
+public class LaunchAppTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("Remote - Main Method");
+	}
+	
+	public void launchApp()
+	{
+		System.out.println("Remote - Launch App");
+	}
+	
+  public void login()
+  {
+  System.out.println("login");
+  }
+
+}
